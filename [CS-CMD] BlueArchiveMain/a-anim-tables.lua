6551297bbbe3ee3d93781d454de5b2cbc10c7072
rPhase <github@rphase.net>
2025-09-27 17:03:38 -0700

    Added GDD NPC Walk + JTF Walk

--- [CS-CMD] BlueArchiveMain/a-anim-tables.lua
+++ [CS-CMD] BlueArchiveMain/a-anim-tables.lua
@@ -5,6 +5,9 @@ ANIM_TABLE_UNIQUE['momoi'] = {
   [CHAR_ANIM_IDLE_HEAD_LEFT] = { anim = "momoi_npc_idle", hand = MARIO_HAND_FISTS },
   [CHAR_ANIM_IDLE_HEAD_RIGHT] = { anim = "momoi_npc_idle", hand = MARIO_HAND_FISTS },
   [CHAR_ANIM_IDLE_HEAD_CENTER] = { anim = "momoi_npc_idle", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_WALKING] = { anim = "momoi_npc_walk", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_START_TIPTOE] = { anim = "momoi_npc_walk", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_TIPTOE] = { anim = "momoi_npc_walk", hand = MARIO_HAND_FISTS },
 }
 ANIM_TABLE_UNIQUE["momoi2"] = ANIM_TABLE_UNIQUE["momoi"]
 ANIM_TABLE_UNIQUE["momoi3"] = ANIM_TABLE_UNIQUE["momoi"]
@@ -13,6 +16,9 @@ ANIM_TABLE_UNIQUE['midori'] = {
   [CHAR_ANIM_IDLE_HEAD_LEFT] = { anim = "midori_npc_idle", hand = MARIO_HAND_OPEN },
   [CHAR_ANIM_IDLE_HEAD_RIGHT] = { anim = "midori_npc_idle", hand = MARIO_HAND_OPEN },
   [CHAR_ANIM_IDLE_HEAD_CENTER] = { anim = "midori_npc_idle", hand = MARIO_HAND_OPEN },
+  [CHAR_ANIM_WALKING] = { anim = "midori_npc_walk", hand = MARIO_HAND_OPEN },
+  [CHAR_ANIM_START_TIPTOE] = { anim = "midori_npc_walk", hand = MARIO_HAND_OPEN },
+  [CHAR_ANIM_TIPTOE] = { anim = "midori_npc_walk", hand = MARIO_HAND_OPEN },
 }
 ANIM_TABLE_UNIQUE['midori2'] = ANIM_TABLE_UNIQUE['midori']
 ANIM_TABLE_UNIQUE['midori3'] = ANIM_TABLE_UNIQUE['midori']
@@ -21,6 +27,9 @@ ANIM_TABLE_UNIQUE['alice'] = {
   [CHAR_ANIM_IDLE_HEAD_LEFT] = { anim = "alice_npc_idle", hand = MARIO_HAND_FISTS },
   [CHAR_ANIM_IDLE_HEAD_RIGHT] = { anim = "alice_npc_idle", hand = MARIO_HAND_FISTS },
   [CHAR_ANIM_IDLE_HEAD_CENTER] = { anim = "alice_npc_idle", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_WALKING] = { anim = "alice_npc_walk", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_START_TIPTOE] = { anim = "alice_npc_walk", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_TIPTOE] = { anim = "alice_npc_walk", hand = MARIO_HAND_FISTS },
 }
 ANIM_TABLE_UNIQUE['alice2'] = ANIM_TABLE_UNIQUE['alice']
 ANIM_TABLE_UNIQUE['alice3'] = ANIM_TABLE_UNIQUE['alice']
@@ -29,6 +38,9 @@ ANIM_TABLE_UNIQUE['yuzu'] = {
   [CHAR_ANIM_IDLE_HEAD_LEFT] = { anim = "yuzu_npc_idle", hand = MARIO_HAND_RIGHT_OPEN },
   [CHAR_ANIM_IDLE_HEAD_RIGHT] = { anim = "yuzu_npc_idle", hand = MARIO_HAND_RIGHT_OPEN },
   [CHAR_ANIM_IDLE_HEAD_CENTER] = { anim = "yuzu_npc_idle", hand = MARIO_HAND_RIGHT_OPEN },
+  [CHAR_ANIM_WALKING] = { anim = "yuzu_npc_walk", hand = MARIO_HAND_RIGHT_OPEN },
+  [CHAR_ANIM_START_TIPTOE] = { anim = "yuzu_npc_walk", hand = MARIO_HAND_RIGHT_OPEN },
+  [CHAR_ANIM_TIPTOE] = { anim = "yuzu_npc_walk", hand = MARIO_HAND_RIGHT_OPEN },
 }
 ANIM_TABLE_UNIQUE['yuzu2'] = ANIM_TABLE_UNIQUE['yuzu']
 ANIM_TABLE_UNIQUE['yuzu3'] = ANIM_TABLE_UNIQUE['yuzu']
@@ -45,6 +57,9 @@ ANIM_TABLE_UNIQUE['jtf'] = {
   [CHAR_ANIM_IDLE_HEAD_LEFT] = "jtf_npc_idle",
   [CHAR_ANIM_IDLE_HEAD_RIGHT] = "jtf_npc_idle",
   [CHAR_ANIM_IDLE_HEAD_CENTER] = "jtf_npc_idle",
+  [CHAR_ANIM_WALKING] = { anim = "jtf_npc_walk", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_START_TIPTOE] = { anim = "jtf_npc_walk", hand = MARIO_HAND_FISTS },
+  [CHAR_ANIM_TIPTOE] = { anim = "jtf_npc_walk", hand = MARIO_HAND_FISTS },
 }
 
 ANIM_TABLE_UNIQUE['mika2'] = {
