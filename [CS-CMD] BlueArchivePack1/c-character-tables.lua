2a9fce5653fd3db1113cebaf8b7bd3bdf53886e7
rPhase <github@rphase.net>
2025-09-28 13:09:49 -0700

    Pack 1 -> Vol 2 (MSS)

--- [CS-CMD] BlueArchivePack1/c-character-tables.lua
+++ [CS-CMD] BlueArchivePack1/c-character-tables.lua
@@ -49,81 +49,7 @@
 --- @type BACharacterTable[]
 BA_CHAR = {}
 
-BA_CHAR[E_MODEL_MOMOI] = {
-  name = "Momoi",
-  description = {
-    "Gamer Twin"
-  },
-  school = "Millennium",
-  credit = "HerosLight",
-  color = { r = 255, g = 119, b = 160 },
-  model = E_MODEL_MOMOI,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_MOMOI,
-  voiceTable = VT_MOMOI,
-  altCostume = {
-    { name = "Momoi (Maid)", model = E_MODEL_MOMOI2, desc = 'Gamer Maid', cmdId = 'momoi2', lifeIcon = TEX_MOMOI2 },
-	{ name = "Momoi (Bunny)", model = E_MODEL_MOMOI3, desc = 'Sexy Gamer Bunny', cmdId = 'momoi3', lifeIcon = TEX_MOMOI3 },
-  },
-  cmdId = 'momoi'
-}
 
-BA_CHAR[E_MODEL_MIDORI] = {
-  name = "Midori",
-  description = {
-    "Gamer Twin"
-  },
-  school = "Millennium",
-  credit = "HerosLight",
-  color = { r = 130, g = 255, b = 106 },
-  model = E_MODEL_MIDORI,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_MIDORI,
-  voiceTable = VT_MIDORI,
-  altCostume = {
-    { name = "Midori (Maid)", model = E_MODEL_MIDORI2, desc = 'Gamer Maid', cmdId = 'midori2', lifeIcon = TEX_MIDORI2 },
-	{ name = "Midori (Bunny)", model = E_MODEL_MIDORI3, desc = 'Sexy Gamer Bunny', cmdId = 'midori3', lifeIcon = TEX_MIDORI3, credit="kzHouston" },
-  },
-  cmdId = 'midori'
-}
-
-BA_CHAR[E_MODEL_YUZU] = {
-  name = "Yuzu",
-  description = {
-    "UZQueen"
-  },
-  school = "Millennium",
-  credit = "HerosLight",
-  color = { r = 233, g = 123, b = 116 },
-  model = E_MODEL_YUZU,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_YUZU,
-  voiceTable = VT_YUZU,
-  altCostume = {
-    { name = "Yuzu (Maid)", model = E_MODEL_YUZU2, desc = 'Gamer Maid', cmdId = 'yuzu2', lifeIcon = TEX_YUZU2 },
-	{ name = "Yuzu (Bunny)", model = E_MODEL_YUZU3, desc = 'Gamer Bunny', cmdId = 'yuzu3', lifeIcon = TEX_YUZU3, credit="MiyuEnjoyer" },
-  },
-  cmdId = 'yuzu'
-}
-
-BA_CHAR[E_MODEL_ALICE] = {
-  name = "Aris",
-  description = {
-    "Super Gamer"
-  },
-  school = "Millennium",
-  credit = "HerosLight",
-  color = { r = 132, g = 150, b = 162 },
-  model = E_MODEL_ALICE,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_ALICE,
-  voiceTable = VT_ALICE,
-  altCostume = {
-    { name = "Alice (Maid)", model = E_MODEL_ALICE2, desc = 'Super Gamer Maid', cmdId = 'alice2', lifeIcon = TEX_ALICE2 },
-    { name = "Alice (Bunny)", model = E_MODEL_ALICE3, desc = 'Your light (bunny suit)', cmdId = 'alice3', lifeIcon = TEX_ALICE3, credit="AllergyDonor" },
-  },
-  cmdId = 'alice'
-}
 
 BA_CHAR[E_MODEL_ARU] = {
   name = "Aru",
@@ -196,41 +122,7 @@ BA_CHAR[E_MODEL_CHISE] = {
   cmdId = 'chise'
 }
 
-BA_CHAR[E_MODEL_HARE] = {
-  name = "Hare",
-  description = {
-    "I love Hare"
-  },
-  school = "Millennium",
-  credit = "hash",
-  color = { r = 233, g = 234, b = 236 },
-  model = E_MODEL_HARE,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_HARE,
-  voiceTable = VT_HARE,
-  altCostume = {
-    { name = "Hare (Camp)", model = E_MODEL_HARE_CAMP, desc = 'She finally touched grass', cmdId = 'hare2', lifeIcon = TEX_HARECMP },
-  },
-  cmdId = 'hare'
-}
 
-BA_CHAR[E_MODEL_HIMARI] = {
-  name = "Himari",
-  description = {
-    "Tensai Bishoujo Hacker"
-  },
-  school = "Millennium",
-  credit = "HerosLight",
-  color = { r = 255, g = 255, b = 255 },
-  model = E_MODEL_HIMARI,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_HIMARI,
-  voiceTable = VT_HIMARI,
-  altCostume = {
-    { name = "Himari (forma Standing)", model = E_MODEL_HIMARI2NW, desc = 'She is no longer messing around', cmdId = 'himari2' },
-  },
-  cmdId = 'himari'
-}
 
 BA_CHAR[E_MODEL_HIYORI] = {
   name = "Hiyori",
@@ -339,23 +231,7 @@ BA_CHAR[E_MODEL_KOKONA] = {
   cmdId = 'kokona'
 }
 
-BA_CHAR[E_MODEL_KOYUKI] = {
-  name = "Koyuki",
-  description = {
-    "Nihahaha"
-  },
-  school = "Millennium",
-  credit = "HerosLight",
-  color = { r = 248, g = 192, b = 214 },
-  model = E_MODEL_KOYUKI,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_KOYUKI,
-  voiceTable = VT_KOYUKI,
-  altCostume = {
-    { name = "Koyuki (Bunny)", model = E_MODEL_KOYUKI2, desc = 'Nihahahaha', cmdId = 'koyuki2', lifeIcon = TEX_KOYUKI2, credit="MiyuEnjoyer" },
-  },
-  cmdId = 'koyuki'
-}
+
 
 BA_CHAR[E_MODEL_MARI] = {
   name = "Mari",
@@ -490,17 +366,3 @@ BA_CHAR[E_MODEL_TSUKUYO] = {
   cmdId = 'tsukuyo'
 }
 
-BA_CHAR[E_MODEL_YUUKA] = {
-  name = "Yuuka",
-  description = {
-    "Mathmetician"
-  },
-  school = "Millennium",
-  credit = "HerosLight",
-  color = { r = 87, g = 84, b = 132 },
-  model = E_MODEL_YUUKA,
-  forceChar = CT_MARIO,
-  lifeIcon = TEX_YUUKA,
-  voiceTable = VT_YUUKA,
-  cmdId = 'yuuka'
-}
--- [CS] BlueArchiveVol2[Millennium]/a-init.lua
+++ [CS] BlueArchiveVol2[Millennium]/a-init.lua
@@ -4,8 +4,44 @@ TEXT_MOD_NAME = "Blue Archive Vol " .. PACK_ID
 -- ================================================================
 -- Custom Models
 -- ================================================================
-
+E_MODEL_MOMOI = smlua_model_util_get_id("momoi_geo")
+E_MODEL_MOMOI2 = smlua_model_util_get_id("momoi2_geo")
+E_MODEL_MOMOI3 = smlua_model_util_get_id("momoi3_geo")
+E_MODEL_MIDORI = smlua_model_util_get_id("midori_geo")
+E_MODEL_MIDORI2 = smlua_model_util_get_id("midori2_geo")
+E_MODEL_MIDORI3 = smlua_model_util_get_id("midori3_geo")
+E_MODEL_YUZU = smlua_model_util_get_id("yuzu_geo")
+E_MODEL_YUZU2 = smlua_model_util_get_id("yuzu2_geo")
+E_MODEL_YUZU3 = smlua_model_util_get_id("yuzu3_geo")
+E_MODEL_ALICE = smlua_model_util_get_id("alice_geo")
+E_MODEL_ALICE2 = smlua_model_util_get_id("alice2_geo")
+E_MODEL_ALICE3 = smlua_model_util_get_id("alice3_geo")
+E_MODEL_HIMARI = smlua_model_util_get_id("himari_geo")
+E_MODEL_HIMARI2NW = smlua_model_util_get_id("himari2nw_geo")
+E_MODEL_YUUKA = smlua_model_util_get_id("yuuka_geo")
+E_MODEL_KOYUKI = smlua_model_util_get_id("koyuki_geo")
+E_MODEL_KOYUKI2 = smlua_model_util_get_id("koyuki2_geo")
+E_MODEL_HARE = smlua_model_util_get_id("hare_geo")
+E_MODEL_HARE_CAMP = smlua_model_util_get_id("hare_camp_geo")
 
 -- ================================================================
 -- Custom Texture Icons
 -- ================================================================
+TEX_MOMOI = get_texture_info("Momoi_Icon")
+TEX_MOMOI2 = get_texture_info("maidmomoiicon")
+TEX_MOMOI3 = get_texture_info("bunnymoiicon")
+TEX_MIDORI = get_texture_info("Midori_Icon")
+TEX_MIDORI2 = get_texture_info("maidmidori_icon")
+TEX_MIDORI3 = get_texture_info("bmidori_icon")
+TEX_YUZU = get_texture_info("Yuzu_Icon")
+TEX_YUZU2 = get_texture_info("maidyuzu_icon")
+TEX_YUZU3 = get_texture_info("byuzu_icon")
+TEX_ALICE = get_texture_info("Portrait_Arisu")
+TEX_ALICE2 = get_texture_info("maidaliceicon")
+TEX_ALICE3 = get_texture_info("alice3_icon")
+TEX_HIMARI = get_texture_info("Himari_Icon")
+TEX_YUUKA = get_texture_info("Yuuka_Icon")
+TEX_KOYUKI = get_texture_info("Koyuki_Icon")
+TEX_KOYUKI2 = get_texture_info("koyuki_bun_icon")
+TEX_HARE = get_texture_info("Hare_Icon")
+TEX_HARECMP = get_texture_info("Hare_Camp_Icon")
