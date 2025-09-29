b0f7ac5bddeb3a9a4b3b5cd618276db44e6ce4b2
rPhase <github@rphase.net>
2025-09-28 17:03:52 -0700

    Pack 3 -> Vol 5 (Hyakkiyako-Shanhaijing-RW)

--- [CS-CMD] BlueArchivePack3/a-init.lua
+++ [CS-CMD] BlueArchivePack3/a-init.lua
@@ -2,49 +2,7 @@ PACK_ID = 3
 TEXT_MOD_NAME = "Blue Archive Pack " .. PACK_ID
 
 -- Custom Models
-E_MODEL_CHERINO = smlua_model_util_get_id("cherino_geo")
-E_MODEL_TOMOE = smlua_model_util_get_id("tomoe_geo")
-E_MODEL_MARINA = smlua_model_util_get_id("marina_geo")
-E_MODEL_KISAKI = smlua_model_util_get_id("kisaki_geo")
-
-E_MODEL_REIJO = smlua_model_util_get_id("reijo_geo")
-
-E_MODEL_SHIZUKO = smlua_model_util_get_id("shizuko_geo")
-
-
-
-
-
-
-E_MODEL_SHUN = smlua_model_util_get_id("shun_geo")
-E_MODEL_CHILD_SHUN = smlua_model_util_get_id("child_shun_geo")
-
-
-
-
-
-
 
 
 -- Custom Texture Icons
-TEX_CHERINO = get_texture_info('cherino_icon')
-TEX_TOMOE = get_texture_info('tomoe_icon')
-TEX_MARINA = get_texture_info('marina_icon')
-TEX_KISAKI = get_texture_info('kisakiicon')
 
-TEX_REIJO = get_texture_info('reijo_icon')
-
-TEX_SHIZUKO = get_texture_info('shizuko_icon')
-
-
-
-
-
-
-
-
-
-
-
-TEX_SHUN = get_texture_info("Shun_Icon")
-TEX_YOUNG_SHUN = get_texture_info("Shun_Young_Icon")
