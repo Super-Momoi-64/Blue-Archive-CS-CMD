1e223a90d8818f2c0ff7bdc910a4db11941aeb35
rPhase <github@rphase.net>
2025-09-28 17:13:15 -0700

    Pack 4 -> Vol 5 (Hyakkiyako-Shanhaijing-RW)

--- [CS-CMD] BlueArchivePack4/a-init.lua
+++ [CS-CMD] BlueArchivePack4/a-init.lua
@@ -2,52 +2,17 @@ PACK_ID = 4
 TEXT_MOD_NAME = "Blue Archive Pack " .. PACK_ID
 
 -- Custom Models
-E_MODEL_RENGE = smlua_model_util_get_id("renge_geo")
-E_MODEL_RENGE2 = smlua_model_util_get_id("renge_swim_geo")
-E_MODEL_KIKYOU = smlua_model_util_get_id("kikyou_geo")
-E_MODEL_KIKYOU2 = smlua_model_util_get_id("kikyou_swim_geo")
-E_MODEL_NAGUSA = smlua_model_util_get_id("nagusa_geo")
-E_MODEL_YUKARI = smlua_model_util_get_id("yukari_geo")
-E_MODEL_YUKARI2 = smlua_model_util_get_id("yukari2_geo")
-
-E_MODEL_WAKAMO = smlua_model_util_get_id("wakamo_geo")
-E_MODEL_WAKAMO2 = smlua_model_util_get_id("wakamo2_geo")
-
-E_MODEL_PINA = smlua_model_util_get_id("pina_geo")
-E_MODEL_UMIKA = smlua_model_util_get_id("umika_geo")
 
 
-
-
-E_MODEL_NODOKA = smlua_model_util_get_id("nodoka_geo")
-E_MODEL_NODOKA2 = smlua_model_util_get_id("nodoka2_geo")
-E_MODEL_RUMI = smlua_model_util_get_id("rumi_geo")
 E_MODEL_SENA = smlua_model_util_get_id("senakzh_geo")
 
 
-E_MODEL_MERU = smlua_model_util_get_id("meru_geo")
+
 E_MODEL_CHIAKI = smlua_model_util_get_id("chiaki_geo")
 
 -- Custom Texture Icons
-TEX_RENGE = get_texture_info("renge_icon")
-TEX_RENGE2 = get_texture_info("rengeswimsuit_icon")
-TEX_KIKYOU = get_texture_info("kikyou_icon")
-TEX_KIKYOU2 = get_texture_info("kikyouswimsuit_icon")
-TEX_NAGUSA = get_texture_info("nagusaicon")
-TEX_YUKARI = get_texture_info("Yukari_Icon")
-TEX_YUKARI2 = get_texture_info("yukariswimsuit_icon")
-
-TEX_WAKAMO = get_texture_info("Wakamo_Icon")
-
-TEX_PINA = get_texture_info("pina_icon")
-TEX_UMIKA = get_texture_info("umika_icon")
 
 
 
-TEX_NODOKA = get_texture_info("nodoka_icon")
-TEX_NODOKA2 = get_texture_info("nodoka2_icon")
-TEX_RUMI = get_texture_info("rumiicon")
 
 
-
-TEX_MERU = get_texture_info("meru_icon")
